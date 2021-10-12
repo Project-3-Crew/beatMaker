823661f4133274df3c086308239eb4f8da8ceeba
spreston <sam.preston11@gmail.com>
2021-10-11 22:43:45 -0500

    Added dependencies.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,8 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Tech, Matchup } = require('../models');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
 
 const resolvers = {
   Query: {
