d9123ccd72500419ef39d956b98c486d6f780a6c
Kayla Morris <moreiskay14@gmail.com>
2021-10-16 19:00:48 -0700

    Deleted uneeded files&installed&imported bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button } from 'antd';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap/Button';
 
 import {
   ApolloClient,
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
         <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
-        > <Button type="primary">Button</Button>
+        >
           <ThoughtForm />
         </div>
         <div className="col-12 col-md-8 mb-3">
