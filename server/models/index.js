2116e3fec8b539631da7d8ed609e8d5ccaa4acb0
hotsoup42 <jose22gw@gmail.com>
2021-10-18 00:15:30 -0700

    added beat to export in /models/index

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,4 @@ const User = require('./User');
 const Thought = require('./Thought');
 const Beat = require('./Beat');
 
-module.exports = { User, Thought };
+module.exports = { User, Thought, Beat};
