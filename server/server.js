a0e53799c54a7de471fbf50cd217005f7c0d3c35
Jeff Lynch <jlynch3@uw.edu>
2021-10-23 07:29:25 -0700

    updated Mongo

--- server/server.js
+++ server/server.js
@@ -15,7 +15,12 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
+const mongoURI = "mongodb+srv://kingami34:Mastery28@cluster0.dlyyn.mongodb.net/myFirstDatabase?retryWrites=true";
 
+mongoose.connect(mongoURI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
 
 server.applyMiddleware({ app });
 
